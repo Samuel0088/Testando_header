90d05f3d6cb341f3bd0edf0eac6b59d4626c3132
LEONARDO CARRILHO MACEDO <045849@americana.fatec.br>
2025-08-27 15:17:50 -0300

    dadsa

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Cabecalho } from './components';
+import { Conteudo } from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <Cabecalho />
+      <Conteudo />
+    </>
+  );
+};
+
+export { App };
